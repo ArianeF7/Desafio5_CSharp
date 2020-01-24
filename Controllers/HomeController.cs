e816b4cc961d1c39bbea6d2063268356d9eead2f
Ariane Lima <arianef.lima@yahoo.com.br>
2020-01-24 15:03:33 -0300

    pORTIFOLIO PESSOAL - ROTAS

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortifolioAriane.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult home()
+        {
+            return View();
+        }
+
+        public ActionResult projetos()
+        {
+            return View();
+        }
+
+
+        public ActionResult skills()
+        {
+            return View();
+        }
+
+        public ActionResult contact()
+        {
+            return View();
+        }
+    }
+}
